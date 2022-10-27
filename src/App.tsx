74dc0942f32a600e39209500e7eda557f3ee5201
StephenZzz <stephenzhao94@163.com>
2022-10-27 22:45:58 +0800

    use hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState }from 'react';
 import logo from './assets/images/logo.svg';
-import robots from './mockdata/robots.json'
+// import robots from './mockdata/robots.json'
 import Robot from './components/Robot';
 import ShoppingCar from './components/ShoppingCar';
 import styles from './App.module.css';
@@ -8,39 +8,27 @@ import styles from './App.module.css';
 interface Props {}
 interface State {
   robotGallery: any[];
+  count: number;
 }
 
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      robotGallery: []
-    }
+const App : React.FC = () => {
 
-  }
+  const [count, setCount] = useState<number>(0)
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then((data) => this.setState( {robotGallery: data}))
-    
-  }
-
-  render() {
     return (
       <div className={styles.app}>
         <div className={styles.appHeader}>
           <img src={logo} className={styles.appLogo} alt="Logo"/>
           <h1>狂拽酷炫叼炸天的名字超级无敌长的的罗伯特Online购物平台</h1>
         </div>
+        <button onClick={()=> {setCount(count + 1)}}>click</button>
+        <span>Count: {count}</span>
         <ShoppingCar/>
-        <div className={styles.robotList}> 
+        {/* <div className={styles.robotList}> 
           {this.state.robotGallery.map( r => <Robot id={r.id} name={r.name} email={r.email}/>)}
-        </div>
+        </div> */}
       </div>
     );
   }
-
-}
 
 export default App;
